e27012a8933a600a7a040a83b1f093a72b04bdf7
szymeo <dev.sgracki@gmail.com>
2025-02-12 22:30:55 +0100

    sth

--- src/lib/games/champdle/components/ChampionGuessingGame.svelte
+++ src/lib/games/champdle/components/ChampionGuessingGame.svelte
@@ -11,6 +11,7 @@
 	};
 	const { game, champions }: Props = $props();
 	let guesses = $state<LolChampion['id'][]>([]);
+	let mounted = $state(false);
 
 	const loadGuesses = () => {
 		const savedGuesses = localStorage.getItem(`guesses-${game.id}`);
@@ -33,30 +34,45 @@
 
 	const has_won_already = $derived(guesses.includes(game.champion));
 
-	onMount(() => loadGuesses());
+	onMount(() => {
+		mounted = true;
+		loadGuesses();
+	});
 </script>
 
-<section
-	class="flex h-full w-full flex-col items-center justify-start overflow-auto"
->
-	<ChampionSelection
-		onSelect={(id: LolChampion['id']) => {
-			onChampionSelected(id);
-		}}
-		autocomplete={[...champions.values()]
-			.map((champion) => ({
-				id: champion.id,
-				name: champion.name,
-				image: champion.avatar,
-			}))
-			.filter((champion) => !guesses.includes(champion.id))}
-	/>
+{#if mounted}
+	<section
+		class="flex h-full w-full flex-col items-center justify-start overflow-auto"
+	>
+		{#if guesses.length === 0}
+			<div
+				class="z-10 bg-black py-2 text-center font-bold text-amber-400"
+			>
+				Who's today's daily challenge champion?
+			</div>
+		{/if}
 
-	{#if has_won_already}
-		<div class="z-10 bg-black py-2 text-center font-bold text-amber-400">
-			You've won today's daily challenge!
-		</div>
-	{/if}
+		<ChampionSelection
+			onSelect={(id: LolChampion['id']) => {
+				onChampionSelected(id);
+			}}
+			autocomplete={[...champions.values()]
+				.map((champion) => ({
+					id: champion.id,
+					name: champion.name,
+					image: champion.avatar,
+				}))
+				.filter((champion) => !guesses.includes(champion.id))}
+		/>
 
-	<GuessesList {guesses} winning_guess={game.champion} {champions} />
-</section>
+		{#if has_won_already}
+			<div
+				class="z-10 bg-black py-2 text-center font-bold text-amber-400"
+			>
+				You've won today's daily challenge!
+			</div>
+		{/if}
+
+		<GuessesList {guesses} winning_guess={game.champion} {champions} />
+	</section>
+{/if}
